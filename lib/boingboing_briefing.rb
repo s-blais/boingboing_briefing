2fa4f145e65e5f3788ef40bff31f95ba9425694d
Stephen Blais <stephen@s-blais.com>
2019-09-20 16:36:59 +0000

    cleanup and CLI UX tweaks

--- lib/boingboing_briefing.rb
+++ lib/boingboing_briefing.rb
@@ -1,12 +1,12 @@
 require 'nokogiri'
 require 'pry'
 require 'open-uri'
+require 'launchy'
 require_relative "./boingboing_briefing/version"
+require_relative "./boingboing_briefing/cli"
+require_relative "./boingboing_briefing/article"
 
 # module BoingboingBriefing
 #   class Error < StandardError; end
 #   # Your code goes here...
 # end
-
-require_relative "./boingboing_briefing/cli"
-require_relative "./boingboing_briefing/article"
--- lib/boingboing_briefing/cli.rb
+++ lib/boingboing_briefing/cli.rb
@@ -14,6 +14,7 @@ class BoingboingBriefing::CLI
     @articles.each.with_index(1) do |article, i|
       puts "#{i}. #{article.title}"
     end
+    puts ""
   end
 
   def present_list_options
@@ -27,7 +28,19 @@ class BoingboingBriefing::CLI
       input = gets.strip.downcase
       if input.to_i > 0
         the_article = @articles[input.to_i-1]
-        puts "#{the_article.title} – #{the_article.author}"
+        puts "-- #{the_article.title} --"
+        puts "#{the_article.author}: #{the_article.teaser.gsub('(more…)','').strip}"
+        puts ""
+        link_input = nil
+        while link_input == nil
+          puts "Interesting enough to pursue further?"
+          puts "Enter \"Y\" to open the full article in your browser.
+          Enter \"N\" to continue"
+          link_input = gets.strip.upcase
+          if link_input == "Y"
+            Launchy.open("#{the_article.url}")
+          end
+        end
       elsif input == "list"
         list_articles
       else "hmm, I don't understand that answer, sorry!"
